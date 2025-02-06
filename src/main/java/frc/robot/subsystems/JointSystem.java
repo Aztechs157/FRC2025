8f7df6d2c56b84f367502613b470549ce39462e4
Katie Ty Silva <pokebladeagle@gmail.com>
2025-02-06 19:27:25 +0100

    Commented JointSystem -KS

--- src/main/java/frc/robot/subsystems/JointSystem.java
+++ src/main/java/frc/robot/subsystems/JointSystem.java
@@ -28,7 +28,12 @@ public class JointSystem extends SubsystemBase implements PosUtils {
   /** Creates a new JointSystem. */
 
   // TODO: Split into two subsystems because it doesn't like defining different motor types
-
+  // This should be fine if you change the variables to not be static - Katie
+  
+  /**
+   * Creates a new Joint subsystem, meant for the elbow and wrist as they should have very similar functionality.
+   * @param isElbow - whether or not this joint is the elbow, primarily for choosing the correct PID and displaying the proper name on shuffleboard
+   */
   public JointSystem(boolean isElbow) {
     if(isElbow){
       motor = new SparkFlex(ElbowConstants.ELBOW_MOTOR_ID, MotorType.kBrushless);
@@ -40,18 +45,37 @@ public class JointSystem extends SubsystemBase implements PosUtils {
     Shuffleboard.getTab("Sensor values").addDouble(isElbow ? "Elbow Encoder" : "Wrist Encoder", this::getPos);
   }
 
+  /**
+   * Runs the motor at a set velocity 
+   * @param velocity - Velocity is actually a percentage of speed, from -1.0 to 1.0
+   */
   public void runMotor(double velocity) {
     motor.set(velocity);
   }
-
+  /**
+   * Get the current speed of the motor
+   * @return - The speed of the motor, in Rotations per Minute
+   */
   private double getMotorVelocity() {
     return motor.getEncoder().getVelocity();
   }
 
+  /**
+   * Returns the current position of the joint.
+   * @return The current value of the absolute encoder, in Rotations 
+   */
   private double getPos() {
     return motor.getAbsoluteEncoder().getPosition();
   }
 
+  /**
+   * Checks if the motor position is within tolerance (as set in Constants) of the desired position, and is moving towards the desired position
+   * 
+   * @param desiredPos the desired position of the joint, in Rotations
+   * @return if the motor is within tolerance
+   * @see frc.robot.Constants
+   * @see frc.utilities.PosUtils#isOscillating(double, double, double, double, double)
+   */
   public boolean isOscillating(double desiredPos) {
     return PosUtils.isOscillating(desiredPos, getPos(), ElevatorConstants.ELEVATOR_POS_TOLERANCE, getMotorVelocity(), ElevatorConstants.ELEVATOR_MOTOR_VELOCITY_TOLERANCE);
   }
